50130c064c120ef8604f2b4271549c67e38cdcca
mishkanchik <mishkanchik594@gmail.com>
2025-11-10 18:02:15 +0200

    froont pay save

--- frontend/CloneNetflixUI/src/App.tsx
+++ frontend/CloneNetflixUI/src/App.tsx
@@ -1,21 +1,25 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import SubscriptionPlansPage from "./pages/SubscriptionPlansPage";
 
 function App() {
-
   return (
-      <>
-          <Routes>
-              <Route path="/">
-                  <Route index element={<HomePage />} />
-              </Route>
-          </Routes>
-      </>
-  )
+    <>
+      <Routes>
+        <Route path="/">
+          <Route index element={<HomePage />} />
+          <Route
+            path="/SubscriptionPlansPage"
+            element={<SubscriptionPlansPage />}
+          />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
