c275f29562076f68fb6984a20d3c4ef70fe180e9
cirillad <burba.2018london@gmail.com>
2026-01-20 19:40:22 +0200

    fix: make avatar optional during registration

--- backend/NetflixClone.Application/UseCases/Auth/Commands/Register/RegisterCommand.cs
+++ backend/NetflixClone.Application/UseCases/Auth/Commands/Register/RegisterCommand.cs
@@ -10,6 +10,6 @@ public class RegisterCommand : IRequest<AuthResponse>
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
     public string ConfirmPassword { get; set; } = string.Empty;
-    public IFormFile Image { get; set; } = null!;
+    public IFormFile? Image { get; set; } = null!;
     public DateOnly DateOfBirth { get; set; }
 }
