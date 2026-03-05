c1246832cb47677407219119bf2d10e7bf3a737b
cirillad <burba.2018london@gmail.com>
2026-03-05 19:27:53 +0200

    Update serch and sort in content

---
+++ backend/NetflixClone.Application/UseCases/Contents/Queries/GetContents/GetContentsQueryValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+
+namespace NetflixClone.Application.UseCases.Contents.Queries.GetContents;
+
+public class GetContentsQueryValidator : AbstractValidator<GetContentsQuery>
+{
+    private readonly string[] _allowedSortFields = { "rating", "year", "title" };
+
+    public GetContentsQueryValidator()
+    {
+
+        RuleFor(x => x.PageNumber)
+            .GreaterThanOrEqualTo(1)
+            .WithMessage("Номер сторінки має бути не менше 1.");
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 100)
+            .WithMessage("Кількість елементів на сторінці має бути від 1 до 100.");
+
+        RuleFor(x => x.SearchTerm)
+            .MaximumLength(100)
+            .WithMessage("Пошуковий запит не може бути довшим за 100 символів.");
+
+        RuleFor(x => x.SortBy)
+            .Must(x => string.IsNullOrEmpty(x) || _allowedSortFields.Contains(x.ToLower()))
+            .WithMessage($"Сортування можливе лише за полями: {string.Join(", ", _allowedSortFields)}.");
+
+        RuleFor(x => x.GenreIds)
+            .Must(x => x == null || x.Count <= 10)
+            .WithMessage("Можна вибрати не більше 10 жанрів одночасно.");
+    }
+}
