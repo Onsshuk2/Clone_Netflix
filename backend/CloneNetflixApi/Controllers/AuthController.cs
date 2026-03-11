b3d786579f10265260dff37cac8f00f4ca6956f4
novakvova <novakvova@gmail.com>
2026-03-11 17:55:35 +0200

    Is godo working app auth google

--- backend/CloneNetflixApi/Controllers/AuthController.cs
+++ backend/CloneNetflixApi/Controllers/AuthController.cs
@@ -1,9 +1,17 @@
-﻿using MediatR;
+﻿using AutoMapper;
+using CloneNetflix.API.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using NetflixClone.Application.UseCases.Auth.Commands.ForgotPassword;
 using NetflixClone.Application.UseCases.Auth.Commands.Login;
 using NetflixClone.Application.UseCases.Auth.Commands.Register;
 using NetflixClone.Application.UseCases.Auth.Commands.ResetPassword;
+using NetflixClone.Domain.Entities;
+using NetflixClone.Infrastructure.Services;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace CloneNetflix.API.Controllers;
 
@@ -12,12 +20,13 @@ namespace CloneNetflix.API.Controllers;
 public class AuthController : ControllerBase
 {
     private readonly IMediator _mediator;
-    
-    private readonly UserManager<User> userManager, 
+    private readonly UserManager<User> _userManager; 
 
-    public AuthController(IMediator mediator)
+    public AuthController(IMediator mediator, UserManager<User> userManager,
+        IMapper mapper)
     {
         _mediator = mediator;
+        _userManager = userManager;
     }
 
     [HttpPost("register")]
@@ -63,6 +72,88 @@ public class AuthController : ControllerBase
             Status = 400,
             IsValid = false,
             Errors = new { Message = "Не вдалося змінити пароль. Перевірте токен або спробуйте ще раз." }
+        });
+    }
+
+    [HttpPost("GoogleLogin")]
+    public async Task<IActionResult> GoogleLogin([FromBody] GoogleLoginRequestModel model)
+    {
+        string token = model.Token;
+        using var httpClient = new HttpClient();
+
+        httpClient.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue("Bearer", token);
+
+        //configuration
+        string userInfo = "https://www.googleapis.com/oauth2/v2/userinfo";
+        var response = await httpClient.GetAsync(userInfo);
+
+        if (!response.IsSuccessStatusCode)
+            return BadRequest(new
+            {
+                Status = 400,
+                IsValid = false,
+                Errors = new { Email = "Помилка реєстрації" }
+            });
+
+        string json = await response.Content.ReadAsStringAsync();
+
+        var googleUser = JsonConvert.DeserializeObject<GoogleAccountModel>(json);
+
+
+        var existingUser = await _userManager.FindByEmailAsync(googleUser!.Email);
+        if (existingUser != null)
+        {
+            var userLoginGoogle = await _userManager.FindByLoginAsync("Google", googleUser.GogoleId);
+
+            if (userLoginGoogle == null)
+            {
+                await _userManager.AddLoginAsync(existingUser, new UserLoginInfo("Google", googleUser.GogoleId, "Google"));
+            }
+            //var jwtToken = await tokenService.CreateTokenAsync(existingUser);
+            return Ok(new
+            {
+                Token = "Good"
+            });
+        }
+        else
+        {
+            var user = new User
+            {
+                Email = googleUser.Email,
+                UserName = googleUser.Email,
+                
+                
+            };//_mapper.Map<User>(googleUser);
+
+            if (!String.IsNullOrEmpty(googleUser.Picture))
+            {
+                //user.Image = await imageService.SaveImageFromUrlAsync(googleUser.Picture);
+            }
+
+            var result = await _userManager.CreateAsync(user);
+            if (result.Succeeded)
+            {
+
+                result = await _userManager.AddLoginAsync(user, new UserLoginInfo(
+                    loginProvider: "Google",
+                    providerKey: googleUser.GogoleId,
+                    displayName: "Google"
+                ));
+
+                await _userManager.AddToRoleAsync(user, "User");
+                //var jwtToken = await tokenService.CreateTokenAsync(user);
+                return Ok(new
+                {
+                    Token = "token"
+                });
+            }
+        }
+        return BadRequest(new
+        {
+            Status = 400,
+            IsValid = false,
+            Errors = new { Email = "Помилка реєстрації" }
         });
     }
 }
